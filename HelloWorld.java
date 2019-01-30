a54fa473a4d2fce1a62352b87207be7e05c0a194
lilyMarvin <lily.marvin@maine.edu>
2019-01-30 11:07:17 -0500

    Hello Bob

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hear ye: important announcement to come");
         System.out.println("Hello World");
+        System.out.println("Hello Bob");
         System.out.println("Farewell!");
     }
 
